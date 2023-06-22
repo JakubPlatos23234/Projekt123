31faaa0c7abfef572eaa8e40d5fe4fd02105873e
JakubPlatos23234 <wwx15466@student.wsb.warszawa.pl>
2023-06-22 23:28:26 +0200

    projekt v2

--- src/main/java/Test/Test_czolgow.java
+++ src/main/java/Test/Test_czolgow.java
@@ -4,10 +4,15 @@ import Czolgi.Tiger;
 import Czolgi.Leopard;
 import Czolgi.Sherman;
 
-
-
+/**
+ * Klasa do testowania czołgów.
+ */
 public class Test_czolgow {
-
+    /**
+     * Metoda główna, która testuje czołgi.
+     *
+     * @param args argumenty wiersza poleceń
+     */
     public static void main(String[] args) {
         Tiger tiger = new Tiger();
         Leopard leopard = new Leopard();
